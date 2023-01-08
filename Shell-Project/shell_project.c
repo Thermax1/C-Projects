1d4bbb0c20fb7096a5f80e1912c92bc5d849b65e
Thermax1 <am15943n@pace.edu>
2023-01-08 21:54:09 +0100

    create shell

--- Shell-Project/shell_project.c
+++ Shell-Project/shell_project.c
@@ -1,6 +1,6 @@
 /*****************************************************************
 Shell Main Functions
-1. Initialze. Read/executre config files. Will change aspects of shell behavior
+1. Initialize. Read/execute config files. Will change aspects of shell behavior
 2. Interpret. Shell reads commands from stdin (interactive or file) and executes the,
 3. Terminate. After commands are executed, shell does shutdown commands, free up memory, and terminates
 
@@ -51,6 +51,61 @@ How to Read a line in C. Self-Learning == Success
 *****************************************************************/
 #define LSH_RL_BUFSIZE 1024 //Defines macros in a C program. Declares constant values or expression.
 char *lsh_read_line(void) {
-   
-   int bufsize = LSH_RL_BUFSIZE
+  
+  int bufsize = LSH_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) *bufsize);
+  int c;
+
+if(!buffer) { //! reverses meaning of operand. Only works for arithmetic or pointer type
+  fprintf(stderr, "lsh: allocation error\n"); //Prints content in file instead of stdout
+  exit(EXIT_FAILURE);
 }
+
+while(1){
+  //Read a character
+  c = getchar(); //Stored as int because EOF is an integer and that's how we check for it
+
+  //If we hit EOF, replace with null character and return
+
+  if(c == EOF || c == '\n') {
+    buffer[position] = '\0'; //This is a null character or terminator
+    return buffer;
+  } else {
+    buffer[position] = c;
+  }
+  position++;
+
+  //If we exceed the buffer, we reallocate
+  if(position >= bufsize) {
+    bufsize += LSH_RL_BUFSIZE;
+    buffer = realloc(buffer, bufsize);
+
+    if(!buffer){
+      fprintf(stderr, "lash: allocation error\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+}
+}
+
+/*****************************************************************
+1. For my reference, C has a getline() function in stdio.h that does most of the code above easily. I will demonstrate it now
+
+char *lsh_read_line(void)
+{
+  char *line = NULL;
+  ssize_t bufsize = 0; //will have getline allocate a buffer for us
+
+  if(getline(&line,&bufsize,stdin) == -1){
+    if (feof(stdin)){
+      exit(EXIT_SUCCESS); //We got an EOF
+    } else {
+      perror("readline");
+      exit(EXIT_FAILURE)
+    }
+  }
+
+  return line;
+}
+*****************************************************************/
