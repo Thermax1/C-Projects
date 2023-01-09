66c96716a1f1cb02fffc2f6498d0253fa0b60b72
Thermax1 <am15943n@pace.edu>
2023-01-09 20:41:21 +0100

    shell code

--- Shell-Project/shell_project.c
+++ Shell-Project/shell_project.c
@@ -95,13 +95,13 @@ while(1){
 char *lsh_read_line(void)
 {
   char *line = NULL;
-  ssize_t bufsize = 0; //will have getline allocate a buffer for us
+  ssize_t bufsize = 0; //will have getline allocate a buffer for us. ssize_t is he same as size_t, but is a signed type. This is TYPE of the result returned by sizeof operator 
 
   if(getline(&line,&bufsize,stdin) == -1){
-    if (feof(stdin)){
+    if (feof(stdin)){ //feof(function) indicates whether the EOF flag is set for the given stream
       exit(EXIT_SUCCESS); //We got an EOF
     } else {
-      perror("readline");
+      perror("readline"); //perror prints an error message to stderr
       exit(EXIT_FAILURE)
     }
   }
@@ -109,3 +109,67 @@ char *lsh_read_line(void)
   return line;
 }
 *****************************************************************/
+
+/*****************************************************************
+Parsing the Line
+1.We will be simplifying, not allowing quotes or backslash escapes into command line arguments
+2. Will use whitespace to separate arguments
+3. We will use whitespace as delimiters
+*****************************************************************/
+
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM "\t\r\n\a"
+char **lsh_split_line(char *line) {
+
+  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize *sizeof(char*));
+  char *token;
+
+  if (!tokens) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, LSH_TOK_DELIM); //breaks string into series of tokens using delimiter. Returns a pointer to first character of a token or null pointer if there is no token.
+  while(token != NULL) {
+    tokens[position] = token; //each pointer is stored in an array (buffer) of character pointers
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += LSH_RL_BUFSIZE;
+      tokens = realloc(tokens, bufsize *sizeof(char*));
+      if(!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
+}
+
+/*****************************************************************
+ How Shells Start Processes
+ 1. Unix computer boots, kernel loads. Kernel starts ONE process called "Init". Apparently, this almost doesn't count
+ 2. fork() system call. OS makes duplicate of process and starts them both running. Parent is original, child is new one.
+3. fork() returns 0 to child process, and it returns to the parent the PID of its child.
+  - Essentially means that the only way for new processes to start is by existing one duplicating itself
+  - Sounds like a problem. What if you want to run a new process? You don't want a copy of the same program. There is a solution.
+4. exec() system call replaces current running program with an entirely new one. OS stops procss, loads up new program, and starts that one in its place.
+  - Processes never return from exec() call unless an error happens
+5. So we have to have an existing proves fork itself into two seperate ones. THEN the child uses exec() to replace itself with a new program.
+6. Parents can do other things and even keep tabs on children using wait() system call.
+7. Finally, my OS class is starting to make sense
+*****************************************************************/
+
+int lsh_launch(char **args) {
+  pid_t pid, wpid; //data type that represents process IDs
+  int status;
+
+  pid = fork();
+  if(pid == 0) {
+    // this is the child process
+  }
+}
